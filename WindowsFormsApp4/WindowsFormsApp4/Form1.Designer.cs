58758d894b53812a64edaba12254335043c456e6
Anton <94841246+ImScarecrow5@users.noreply.github.com>
2026-04-10 06:56:39 +0300

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button DrawKochButton;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.DrawKochButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(600, 300);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.DrawKochButton.Location = new System.Drawing.Point(250, 330);
+            this.DrawKochButton.Name = "DrawKochButton";
+            this.DrawKochButton.Size = new System.Drawing.Size(120, 30);
+            this.DrawKochButton.TabIndex = 1;
+            this.DrawKochButton.Text = "Кривая Коха";
+            this.DrawKochButton.UseVisualStyleBackColor = true;
+            this.DrawKochButton.Click += new System.EventHandler(this.DrawKochButton_Click);
+            this.ClientSize = new System.Drawing.Size(624, 381);
+            this.Controls.Add(this.DrawKochButton);
+            this.Controls.Add(this.pictureBox);
+            this.Text = "Кривая Коха";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawKochButton_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+                PointF start = new PointF(50, pictureBox.Height - 100);
+                PointF end = new PointF(pictureBox.Width - 50, pictureBox.Height - 100);
+                DrawKoch(g, start, end, 4); // глубина 4
+            }
+            pictureBox.Image = bmp;
+        }
+
+        private void DrawKoch(Graphics g, PointF p1, PointF p2, int depth)
+        {
+            if (depth == 0)
+            {
+                g.DrawLine(Pens.Black, p1, p2);
+            }
+            else
+            {
+                float dx = p2.X - p1.X;
+                float dy = p2.Y - p1.Y;
+                float thirdX = dx / 3;
+                float thirdY = dy / 3;
+                PointF pA = new PointF(p1.X + thirdX, p1.Y + thirdY);
+                PointF pB = new PointF(p1.X + 2 * thirdX, p1.Y + 2 * thirdY);
+                float angle = (float)Math.Atan2(dy, dx) - (float)(Math.PI / 3);
+                float len = (float)Math.Sqrt(thirdX * thirdX + thirdY * thirdY);
+                float peakX = pA.X + len * (float)Math.Cos(angle);
+                float peakY = pA.Y + len * (float)Math.Sin(angle);
+                PointF peak = new PointF(peakX, peakY);
+                DrawKoch(g, p1, pA, depth - 1);
+                DrawKoch(g, pA, peak, depth - 1);
+                DrawKoch(g, peak, pB, depth - 1);
+                DrawKoch(g, pB, p2, depth - 1);
+            }
+        }
+    }
+}
